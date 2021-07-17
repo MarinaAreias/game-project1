047978fb24a799a69dc1587a1a81cc7326d831fd
Marina Areias <marinasareias@gmail.com>
2021-07-17 11:01:54 +0200

    Power ups
    -created power ups
    - when impact with power ups life increases

--- classes/game.js
+++ classes/game.js
@@ -2,56 +2,174 @@ class Game {
   constructor() {
     this.bg = new Image();
     this.bg.src = "../imgs/space-2.jpg";
-
+    // life
+    this.heart = {
+      image: new Image(),
+      width: 20,
+      height: 20,
+      startX: 20,
+      startY: 20,
+    };
+    this.heart.image.src = "../imgs/heart.png";
+    // game
+    this.isGameOn = true;
     // player
     this.player = new Player();
-    this.isGameOn = true;
     // enemies
     this.enemiesArr = [];
     // powerUp
-    this.powerUp = new PowerUp();
+    this.powerUpArr = [];
+    this.enemiesGenerationSpeed = 5000;
+    this.levelUpSpeed = 5000;
+    this.score = 0;
   }
 
   generateEnemies = () => {
-    // if the array is empty  || the last pipe collides with the middle of canvas
+    // if the array is empty
+
+    // create a enemy
+    let enemy = new Enemy("../imgs/meteo.jpg");
+    // add the enemy to the Arr
+    this.enemiesArr.push(enemy);
+  };
+
+  drawScore = () => {
+    ctx.font = "20px serif";
+    ctx.fillStyle = "white";
+    ctx.fillText(this.score, canvas.width - 50, 50);
+  };
+
+  removeEnemy = (enemy) => {
+    const enemyIndex = this.enemiesArr.indexOf(enemy);
+    this.enemiesArr.splice(enemyIndex, 1);
+  };
+
+  // POWER UP
+  // generate POWER
+  generatePower = () => {
+    // if the array is empty  || the last PowerUp collides with the middle of canvas
     if (
-      !this.enemiesArr.length ||
-      this.enemiesArr[this.enemiesArr.length - 1].y > canvas.height / 2
+      !this.powerUpArr.length ||
+      this.powerUpArr[this.powerUpArr.length - 1].y > canvas.height / 2
     ) {
-      // create a enemy
-      let enemy = new Enemy("../imgs/meteo.jpg");
+      // create a powerUp
+      let powerUp = new PowerUp("../imgs/life.png");
       // add the enemy to the Arr
-      this.enemiesArr.push(enemy);
+      this.powerUpArr.push(powerUp);
     }
   };
 
+  removePowerUp = (powerUp) => {
+    const powerUpIndex = this.powerUpArr.indexOf(powerUp);
+    this.powerUpArr.splice(powerUpIndex, 1);
+  };
+
+  // <<<<<<<< LIFE >>>>>>
+  drawLifes = () => {
+    for (let index = 0; index < this.player.lifes; index++) {
+      ctx.drawImage(
+        this.heart.image,
+        this.heart.startX + this.heart.width * index,
+        this.heart.startY,
+        this.heart.width,
+        this.heart.height
+      );
+    }
+  };
+
+  addLifeCheck = () => {
+    this.powerUpArr.forEach((eachPowerUp) => {
+      if (this.player.playerPowerUpCollision(eachPowerUp)) {
+        // we need to remove the PowerUp or it creates several collitions
+        this.player.lifes++;
+        this.removePowerUp(eachPowerUp);
+        if (this.player.lifes === 0) {
+          this.isGameOn = false;
+        }
+      }
+    });
+  };
+
+  // for the collitions and to REMOVE life
+  looseLifeCheck = () => {
+    this.enemiesArr.forEach((eachEnemy) => {
+      if (this.player.playerEnemyCollision(eachEnemy)) {
+        // we need to remove the enemy or it creates several collitions
+        this.player.lifes--;
+        this.removeEnemy(eachEnemy);
+        if (this.player.lifes === 0) {
+          this.isGameOn = false;
+          canvas.style.display = "none";
+          gameoverScreen.style.display = "flex";
+        }
+      }
+    });
+  };
+
   // first thing to be created
-  gameloop = () => {
+  gameloop = (enemiesStamp = 0, levelUpStamp = 0, scoreTimestamp = 0) => {
+    //
     // 1. clearing canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-
+    //
     // 2.movements of elements or other action
-    // enemy
-    this.generateEnemies();
+
+    // powerUp
+    this.generatePower();
+
     // enemyArr
     this.enemiesArr.forEach((eachEnemy) => {
       eachEnemy.enemyMove();
     });
+    // POWERUPArr
+    this.powerUpArr.forEach((eachPowerUp) => {
+      eachPowerUp.powerUpMove();
+    });
+
+    // add life
+    this.addLifeCheck();
+    // loose life
+    this.looseLifeCheck();
+
+    //
     // 3. drawing elements
     ctx.drawImage(this.bg, 0, 0, canvas.width, canvas.height);
 
     // player
     this.player.drawPlayer();
+
     // enemy Arr to draw enemies
     this.enemiesArr.forEach((eachEnemy) => {
       eachEnemy.drawEnemy();
     });
-    // powerUp
-    this.powerUp.drawPowerUp();
 
+    // POWERUP arr
+    this.powerUpArr.forEach((eachPowerUp) => {
+      eachPowerUp.drawPowerUp();
+    });
+
+    // LIFE
+    this.drawLifes();
+    //
+    // SCORE
+    this.drawScore();
     // 4. request animation
     if (this.isGameOn) {
-      requestAnimationFrame(this.gameloop);
+      requestAnimationFrame((timestamp) => {
+        if (timestamp - enemiesStamp > this.enemiesGenerationSpeed) {
+          this.generateEnemies();
+          enemiesStamp = timestamp;
+        }
+        if (timestamp - levelUpStamp > this.levelUpSpeed) {
+          this.enemiesGenerationSpeed *= 0.5;
+          levelUpStamp = timestamp;
+        }
+        if (timestamp - scoreTimestamp > 1000) {
+          this.score++;
+          scoreTimestamp = timestamp;
+        }
+        this.gameloop(enemiesStamp, levelUpStamp, scoreTimestamp);
+      });
     }
   };
 }
--- classes/powerup.js
+++ classes/powerup.js
@@ -1,21 +1,21 @@
-class Enemy {
+class PowerUp {
   constructor(srcURL) {
     this.image = new Image();
     this.image.src = srcURL;
-    this.width = 20;
     this.height = 20;
+    this.width = 20;
     this.x = Math.floor(Math.random() * (canvas.width - this.width));
     this.y = this.height * -1;
     this.speed = 2;
   }
 
-  // draw enemy
-  drawEnemy = () => {
+  // draw power up
+  drawPowerUp = () => {
     ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
   };
 
   //   make enemy move down
-  enemyMove = () => {
+  powerUpMove = () => {
     this.y += this.speed;
   };
 }
