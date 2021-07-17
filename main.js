4a16329132ad8084d9896da3495d182f43d10390
Marina Areias <marinasareias@gmail.com>
2021-07-17 11:22:56 +0200

    started restart page

--- main.js
+++ main.js
@@ -17,23 +17,32 @@ const restartBtn = document.querySelector("#restart-btn");
 // screen
 // start screen
 const splashScreen = document.querySelector("#splash-screen");
+// RESTART SCREEN
+let gameoverScreen = document.querySelector("#gameover-screen");
 
 // main game global variable
 let gameObj;
 
 // EVENT LISTENERS
 // START BTN
-
 startBtn.addEventListener("click", () => {
   //show canvas
   canvas.style.display = "block";
   //need to hide start screen
   splashScreen.style.display = "none";
-
   // here game is created using classes => see Game in classes folder
   gameObj = new Game();
 
   gameObj.gameloop();
+});
+
+// RESTART BTN
+restartBtn.addEventListener("click", () => {
+  // show the canvas DOM element
+  canvas.style.display = "block"; // hide the splash screen DOM element
+  gameoverScreen.style.display = "none"; // here we need to create the game
+  gameObj = new Game(); // => game will have all properties and methods of Game class!
+  gameObj.gameLoop(); // => invoke method // here we need to start the game
 });
 
 // Event listener for player moove
